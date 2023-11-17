f6faf40d4a6b222df66bfdeecdd5086ca7612c7f
berkaykrc <berkay.kirci@gmail.com>
2023-11-17 21:54:12 +0300

    Refactor TaskViewSet to handle validation errors in retrieve method

--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -1,11 +1,14 @@
 from .serializers import TaskSerializer
 from .models import Task
-from rest_framework import viewsets, decorators, response, reverse
+from rest_framework import viewsets, decorators, response
+from rest_framework.reverse import reverse
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
+from django.core.exceptions import ValidationError
 
 # Create your views here.
+
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
@@ -16,19 +19,19 @@ class TaskViewSet(viewsets.ModelViewSet):
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
 
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        try:
+            duration = instance.duration
+        except ValidationError as e:
+            return response.Response({'error': str(e)}, status=400)
+        serializer = self.get_serializer(instance)
+        return response.Response(serializer.data)
+
     @decorators.action(detail=True, methods=['post'])
-    def assign_task(self, request, pk=None):
+    def assign_task(self, request):
         task = self.get_object()
         user = request.user
         task.assigned.add(user)
         task.save()
         return response.Response({'status': 'task assigned'})
-
-@decorators.api_view(['GET'])
-def api_root(request, format=None):
-    return response.Response({
-        'users': reverse.reverse('user-list', request=request, format=format),
-        'tasks': reverse.reverse('task-list', request=request, format=format),
-        'profiles': reverse.reverse('profile-list', request=request, format=format),
-    })
-
