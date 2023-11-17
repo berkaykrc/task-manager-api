34a19af50ad119c8f73a8a9649594599bb552a25
berkaykrc <berkay.kirci@gmail.com>
2023-11-17 21:54:49 +0300

    Add JWT authentication and IsAuthenticated permission to ProfileViewSet.

--- taskmanager/profiles/views.py
+++ taskmanager/profiles/views.py
@@ -3,7 +3,11 @@
 from rest_framework import viewsets
 from .models import Profile
 from .serializers import ProfileSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
