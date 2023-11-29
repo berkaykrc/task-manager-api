31402e4f09816cabbd092f2dbd936df4965fee41
berkaykrc <berkay.kirci@gmail.com>
2023-11-30 01:34:19 +0300

    Add UserViewSet to views.py

--- taskmanager/profiles/views.py
+++ taskmanager/profiles/views.py
@@ -1,13 +1,21 @@
 # views.py
 
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
 from rest_framework import viewsets
 from .models import Profile
 from .serializers import ProfileSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
