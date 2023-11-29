0847d03a4e7fb37c009c4797176b36e379e21490
berkaykrc <berkay.kirci@gmail.com>
2023-11-30 01:34:14 +0300

    Add image field to Profile and User serializers

--- taskmanager/profiles/serializers.py
+++ taskmanager/profiles/serializers.py
@@ -1,6 +1,7 @@
 # serializers.py
 from rest_framework import serializers
 from .models import Profile
+from django.contrib.auth.models import User
 
 
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
@@ -9,7 +10,22 @@ class ProfileSerializer(serializers.HyperlinkedModelSerializer):
         read_only=True,
         view_name='task-detail'
     )
+    image_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
-        fields = ['user', 'image', 'tasks']
+        fields = ['user', 'image_url', 'tasks']
+
+    def get_image_url(self, obj):
+        return obj.image.url if obj.image else None
+
+
+class UserSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'image']
+
+    def get_image(self, obj):
+        return obj.profile.image.url if obj.profile and obj.profile.image else None
