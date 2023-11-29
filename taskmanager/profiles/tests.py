e6c1b6cd04b43254effb7ae7ba89c7aa9fb8ab2a
berkaykrc <berkay.kirci@gmail.com>
2023-11-30 01:34:48 +0300

    Add profile tests for list and retrieve endpoints

--- taskmanager/profiles/tests.py
+++ taskmanager/profiles/tests.py
@@ -1,3 +1,22 @@
+from traceback import print_tb
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from rest_framework import status
 
-# Create your tests here.
+class ProfileViewSetTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.force_authenticate(user=self.user)
+
+    def test_list_profiles(self):
+        response = self.client.get('/profiles/')
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_profile(self):
+        profile = self.user.profile
+        print(profile.id)
+        response = self.client.get(f'/profiles/{profile.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
