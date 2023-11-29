1e8d489a899a4a96d1824041d4a6544ec3e4022b
berkaykrc <berkay.kirci@gmail.com>
2023-11-30 01:32:48 +0300

    Refactor UserSerializer import in tasks/serializers.py

--- taskmanager/tasks/serializers.py
+++ taskmanager/tasks/serializers.py
@@ -1,18 +1,8 @@
 # create tasks seraializer.py
 from rest_framework import serializers
 from .models import Task
-from django.contrib.auth.models import User
+from profiles.serializers import UserSerializer
 
-
-class UserSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'image']
-
-    def get_image(self, obj):
-        return obj.profile.image.url if obj.profile and obj.profile.image else None
 
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
