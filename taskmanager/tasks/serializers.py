c799b5086be3b5c1eb220c5543359e0c3a9c540e
berkaykrc <berkay.kirci@gmail.com>
2023-12-01 01:13:53 +0300

    Add validation for start_date and end_date in TaskSerializer

--- taskmanager/tasks/serializers.py
+++ taskmanager/tasks/serializers.py
@@ -4,7 +4,6 @@ from .models import Task
 from profiles.serializers import UserSerializer
 
 
-
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
     assigned = UserSerializer(many=True, read_only=True)
     creator = UserSerializer(read_only=True)
@@ -13,3 +12,9 @@ class TaskSerializer(serializers.HyperlinkedModelSerializer):
         model = Task
         fields = ['id', 'creator', 'name', 'description', 'assigned',
                   'start_date', 'end_date', 'priority', 'status', 'duration']
+
+    def validate(self, data):
+        if data['start_date'] > data['end_date']:
+            raise serializers.ValidationError(
+                "end_date must be after start_date")
+        return data
