df1cd4a5b7dae2b4716972fa1b7a82d0c10c62e9
berkaykrc <berkay.kirci@gmail.com>
2023-12-01 01:14:44 +0300

    Remove unnecessary print statements in profile tests

--- taskmanager/profiles/tests.py
+++ taskmanager/profiles/tests.py
@@ -1,4 +1,3 @@
-from traceback import print_tb
 from django.test import TestCase
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
@@ -12,11 +11,9 @@ class ProfileViewSetTestCase(TestCase):
 
     def test_list_profiles(self):
         response = self.client.get('/profiles/')
-        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_retrieve_profile(self):
         profile = self.user.profile
-        print(profile.id)
         response = self.client.get(f'/profiles/{profile.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
