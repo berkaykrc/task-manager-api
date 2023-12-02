fdda74994d8962245d29fffaa005946a47c6a931
berkaykrc <berkay.kirci@gmail.com>
2023-12-03 01:00:26 +0300

    Add TaskAdmin class to tasks/admin.py

--- taskmanager/tasks/admin.py
+++ taskmanager/tasks/admin.py
@@ -1,4 +1,15 @@
+from re import T
 from django.contrib import admin
 from .models import Task
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date', 'duration', 'status', 'priority', 'creator', 'display_assigned')
+    list_filter = ('status', 'priority', 'creator')
+    search_fields = ('name', 'description')
+    ordering = ('-start_date',)
+
+    def display_assigned(self, obj):
+        return ", ".join([assigned.username for assigned in obj.assigned.all()])
+    display_assigned.short_description = 'Assigned To'
+
+admin.site.register(Task,TaskAdmin)
