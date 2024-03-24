a6014c527a8b8fad6653d9741d6097f3926445c8
berkaykrc <berkay.kirci@gmail.com>
2024-03-24 22:30:55 +0300

    Add custom method to display assigned users in TaskAdmin

--- taskmanager/tasks/admin.py
+++ taskmanager/tasks/admin.py
@@ -1,4 +1,5 @@
 """Admin module for Task app."""
+
 from django.contrib import admin
 
 from .models import Task
@@ -7,6 +8,20 @@ from .models import Task
 class TaskAdmin(admin.ModelAdmin):
     """
     Admin View for Task
+
+    This class defines the admin interface for the Task model.
+    It specifies the list display, list filter, search fields, and ordering options.
+    It also defines a custom method to display the assigned users.
+
+    Attributes:
+        list_display (tuple): A tuple of field names to be displayed in the list view.
+        list_filter (tuple): A tuple of field names to be used for filtering the list view.
+        search_fields (tuple): A tuple of field names to be used for searching the list view.
+        ordering (str): The field name to be used for ordering the list view.
+
+    Methods:
+        display_assigned(obj): Returns a string representation of the assigned users.
+
     """
 
     list_display = (
