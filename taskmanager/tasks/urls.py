89de79e2826705803ff68c45f642f17d27b63444
berkaykrc <berkay.kirci@gmail.com>
2024-03-24 22:36:39 +0300

    Refactor URL patterns for tasks app

--- taskmanager/tasks/urls.py
+++ taskmanager/tasks/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path, include
+"""
+This module defines the URL patterns for the tasks app.
+
+It includes a router that automatically generates the URL patterns for the TaskViewSet.
+"""
+
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
 from .views import TaskViewSet
 
 router = DefaultRouter()
-router.register(r'', TaskViewSet)
+router.register(r"", TaskViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
