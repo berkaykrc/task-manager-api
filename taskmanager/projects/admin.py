c58839c43793ac3f057d4eaee41ee1b8050cacb3
berkaykrc <berkay.kirci@gmail.com>
2024-06-18 00:08:36 +0300

    chore: Update ProjectAdmin configuration for the Project model

--- taskmanager/projects/admin.py
+++ taskmanager/projects/admin.py
@@ -1,3 +1,7 @@
+"""
+This module contains the admin configuration for the Project model.
+"""
+
 from django.contrib import admin
 from tasks.admin import TaskInline
 
@@ -5,10 +9,24 @@ from .models import Project
 
 
 class ProjectAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Project model.
+
+    This class defines the display, filtering, search, and inline editing options for the Project model in the Django admin interface.
+
+    Attributes:
+        list_display (tuple): A tuple of field names to be displayed in the admin list view.
+        list_filter (tuple): A tuple of field names to be used for filtering in the admin list view.
+        search_fields (list): A list of field names to be used for searching in the admin list view.
+        inlines (list): A list of inline models to be displayed and edited on the Project admin page.
+        filter_horizontal (tuple): A tuple of field names to be displayed as horizontal filter widgets in the admin interface.
+
+    """
+
     list_display = ('name', 'description', 'start_date', 'end_date', 'owner')
     list_filter = ('start_date', 'end_date')
     search_fields = ['name', 'description']
-    inlines = [TaskInline]  # Add this line
+    inlines = [TaskInline]
     filter_horizontal = ('users',)
 
 
