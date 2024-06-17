9932eddda2bd106108196c84c3f4910614c51b65
berkaykrc <berkay.kirci@gmail.com>
2024-06-18 00:05:19 +0300

    Refactor profiles URLs to remove unused imports and static file serving

--- taskmanager/profiles/urls.py
+++ taskmanager/profiles/urls.py
@@ -6,8 +6,6 @@ This file has the URLs for the Profiles app.
 Attributes:
     urlpatterns (list): The URLs for the Profiles app.
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
@@ -21,7 +19,3 @@ router.register(r"", ProfileViewSet)
 urlpatterns = [
     path("", include(router.urls)),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
