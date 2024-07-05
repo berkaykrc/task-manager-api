bee9eb68d73d1efc24a19df5a2ee53e6dfceb025
berkaykrc <berkay.kirci@gmail.com>
2024-07-05 23:58:15 +0300

    chore: Add SharedFilesInline in admin.py for displaying shared files in the admin view

--- taskmanager/files/admin.py
+++ taskmanager/files/admin.py
@@ -3,3 +3,9 @@ from django.contrib import admin
 from .models import SharedFile
 
 admin.site.register(SharedFile)
+
+
+class SharedFilesInline(admin.TabularInline):
+    """Inline class for displaying tasks in the Project admin view."""
+    model = SharedFile
+    extra = 0
